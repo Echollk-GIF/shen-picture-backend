678d1248199213d13a8dbff1b0958d9ec69e48f9
chuanlong <841290355@qq.com>
2025-12-31 20:56:41 +0800

    feat: 初始化项目

---
+++ src/main/java/com/chuanlong/shenpicturebackend/ShenPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chuanlong.shenpicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShenPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShenPictureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chuanlong/shenpicturebackend/ShenPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuanlong.shenpicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShenPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
