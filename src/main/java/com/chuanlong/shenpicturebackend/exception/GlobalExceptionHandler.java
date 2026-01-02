ab604016a52d3e56d86b52d8b0683c8e65e2573f
chuanlong <841290355@qq.com>
2026-01-02 20:32:25 +0800

    feat: 新增通用基类封装

---
+++ src/main/java/com/chuanlong/shenpicturebackend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.chuanlong.shenpicturebackend.exception;
+
+import com.chuanlong.shenpicturebackend.common.BaseResponse;
+import com.chuanlong.shenpicturebackend.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> businessExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
+
